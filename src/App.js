3185532d9fdcbcc1f8ec1429e0bd2338045b461e
Blackstate <josemiguelmneyra@gmail.com>
2020-10-28 19:06:49 +0800

    initial files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
+import { fetchSynonyms } from './fetchData';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const getSynonyms = async () => {
+      const data = await fetchSynonyms('amazing');
+      console.log(data);
+    };
+
+    getSynonyms();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
